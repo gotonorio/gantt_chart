d4b2093d2d601c6fe5867d6a31418c528244d44b
Goto Norio <gotonorio@gmail.com>
2022-08-12 08:47:06 +0900

    matplotlibによるグラフを追加

--- ltrp.py
+++ ltrp.py
@@ -4,32 +4,75 @@
 
 import plotly.express as px
 import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
 
 
-# データ
-df = pd.DataFrame([
-    dict(Task='アンケート', Start='2022-10-01', Finish='2022-10-31', Resource='理事会'),
-    dict(Task='コンサル選定', Start='2022-09-01', Finish='2022-12-10', Resource='理事会'),
-    dict(Task='仕様書作成', Start='2023-01-15', Finish='2023-07-25', Resource='設計施工管理'),
-    dict(Task='施工業者選定', Start='2023-08-01', Finish='2023-11-15', Resource='理事会'),
-    dict(Task='着工準備', Start='2023-11-15', Finish='2024-01-15', Resource='施工業者'),
-    dict(Task='施工管理', Start='2024-01-15', Finish='2024-10-20', Resource='設計施工管理'),
-    dict(Task='修繕工事実施', Start='2024-01-15', Finish='2024-10-20', Resource='施工業者'),
-])
+def main(graph_type='px') -> None:
+    # データ
+    df = pd.DataFrame([
+        dict(Task='アンケート', Start='2022-10-01', Finish='2022-10-31', Resource='理事会'),
+        dict(Task='コンサル選定', Start='2022-09-01', Finish='2022-12-10', Resource='理事会'),
+        dict(Task='仕様書作成', Start='2023-01-15', Finish='2023-07-25', Resource='設計施工管理'),
+        dict(Task='施工業者選定', Start='2023-08-01', Finish='2023-11-15', Resource='理事会'),
+        dict(Task='着工準備', Start='2023-11-15', Finish='2024-01-15', Resource='施工業者'),
+        dict(Task='施工管理', Start='2024-01-15', Finish='2024-10-20', Resource='設計施工管理'),
+        dict(Task='修繕工事実施', Start='2024-01-15', Finish='2024-10-20', Resource='施工業者'),
+    ])
 
-# 作図
-fig = px.timeline(df, x_start="Start", x_end="Finish", y="Task", color="Resource",
-                  title='第2回大規模修繕工事 工程表（案）（2022年9月〜2024年11月）', height=450, width=950)
-
-# グラフ全体とホバーのフォントサイズ変更
-fig.update_layout(font_size=14, hoverlabel_font_size=14, hoverlabel_font_color='white',
-                  xaxis_title='年月', yaxis_title='作業項目',
-                  xaxis_title_font_size=14, yaxis_title_font_size=14)
-
-# 枠線とグリッド線の設定（x軸グリッドは5日表示=5x24x60x60x1000）
-fig.update_xaxes(linecolor='black', gridcolor='gray',mirror=True, tickformat="%y/%m", dtick = 'M2')
-fig.update_yaxes(linecolor='black', gridcolor='gray',mirror=True, autorange='reversed')
+    if graph_type == 'px':
+        plot_by_plotly(df)
+    else:
+        # DataFrameをlistに変換。
+        data_list = df_to_list(df)
+        plot_by_mpl(data_list)
 
 
-# ガントチャート表示
-fig.show()
+def plot_by_plotly(df) -> None:
+    """ plotlyによるガントチャート """
+    fig = px.timeline(df, x_start="Start", x_end="Finish", y="Task", color="Resource",
+                      title='第2回大規模修繕工事 工程表（案）（2022年9月〜2024年11月）', height=450, width=950)
+    # グラフ全体とホバーのフォントサイズ変更
+    fig.update_layout(font_size=14, hoverlabel_font_size=14, hoverlabel_font_color='white',
+                      xaxis_title='年月', yaxis_title='作業項目',
+                      xaxis_title_font_size=14, yaxis_title_font_size=14)
+    # 枠線とグリッド線の設定（x軸グリッドは5日表示=5x24x60x60x1000）
+    fig.update_xaxes(linecolor='black', gridcolor='gray',mirror=True, tickformat="%y/%m", dtick = 'M2')
+    fig.update_yaxes(linecolor='black', gridcolor='gray',mirror=True, autorange='reversed')
+    # ガントチャート表示
+    fig.show()
+
+
+def df_to_list(df) -> list:
+    l_2d = df.values.tolist()
+    return l_2d
+
+
+def plot_by_mpl(data_list):
+    """ 無理やりmatplotlibでガントチャート """
+    data_list.reverse()
+    # データの取り出し。
+    titles, start, end, res = list(zip(*data_list))
+    print(titles)
+
+    # pyplot内でDatesとして扱われるように変換
+    edate, bdate = [mdates.date2num(item) for item in (end, start)]
+    # 日本語に対応
+    plt.rcParams['font.family'] = 'Ricty Diminished'
+
+    fig, ax = plt.subplots(figsize=(12, 6))
+    # タイトル領域
+    fig.subplots_adjust(left=0.1, right=0.95)
+    # 作図 
+    ax.barh(y=titles, width=edate - bdate, left=bdate, height=0.5)
+    #  横軸目盛の表示形式をdateに変更する
+    ax.xaxis_date()
+
+    # 描画
+    plt.savefig("hoge.png")
+    plt.show()
+
+
+if __name__ == "__main__":
+    """ matplotlibでグラフ表示の場合は、main('plt')とする。 """
+    main()
